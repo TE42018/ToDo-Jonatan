d64d1e7276568f5ddef7692f125113e10d84209f
JonatanW <jonatan.walett@elev.ga.ntig.se>
2019-01-22 10:27:09 +0100

    copy project

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'My ToDo app';
+  todos = [
+    {
+      label: 'köp bil ',
+      priority: 3,
+      done: false,
+    },
+    {
+      label: 'köp redbull ',
+      priority: 5,
+      done: true,
+    },
+    {
+      label: 'köp mat ',
+      priority: 4,
+      done: false,
+    },
+    {
+      label: 'köp hus ',
+      priority: 1,
+      done: false,
+    },
+  ];
+
+  addTodo(newTodoLabel) {
+    var newTodo = {
+      label: newTodoLabel,
+      priority: 1,
+      done: false
+    };
+    this.todos.push(newTodo);
+  }
+  deleteTodo(ToDo) {
+    this.todos = this.todos.filter(t => t.label !== ToDo.label);
+  }
+}
